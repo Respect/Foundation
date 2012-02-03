cf6bc4c43c6e7e7bd3bf3bffecaea03672a75144
Henrique Moody <henriquemoody@gmail.com>
2012-02-03 18:46:21 -0200

    Updated file composer-package.php with the functions of common-inc.php.

--- bin/common-inc.php
+++ bin/common-inc.php
@@ -5,6 +5,9 @@ if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
     exit(1);
 }
 
+chdir(__DIR__);
+date_default_timezone_set('UTC');
+
 /**
  * @param   string $message 
  * @param   resource $stream 
@@ -23,7 +26,10 @@ function write($message, $stream = STDOUT)
 function writeln($message = '', $stream = STDOUT)
 {
     if (is_array($message)) {
-        foreach ($message as $value) {
+        foreach ($message as $key => $value) {
+            if (is_string($key)) {
+                $value = vsprintf($key, (array) $value);
+            }
             writeln($value, $stream);
         }
     } else {
@@ -86,3 +92,71 @@ function increase_version($version, $type = 'patch')
     }
     return "{$major_version}.{$minor_version}.{$patch_version}";
 }
+
+/**
+ * @author  umbrae@gmail.com
+ * @link    http://www.php.net/manual/en/function.json-encode.php#80339
+ * @param   mixed $data
+ * @return  string
+ */
+function json_encode_formatted($data)
+{
+    if (defined('JSON_PRETTY_PRINT')) {
+        return json_encode($data, JSON_PRETTY_PRINT);
+    }
+
+    $json       = json_encode($data);
+    $tab        = '    ';
+    $formated   = '';
+    $level      = 0;
+    $in_string  = false;
+    $length     = strlen($json);
+
+    for ($c = 0; $c < $length; $c++) {
+        $char = $json[$c];
+        switch ($char) {
+            case '{':
+            case '[':
+                if (!$in_string) {
+                    $formated .= $char . "\n" . str_repeat($tab, $level + 1);
+                    $level++;
+                } else {
+                    $formated .= $char;
+                }
+                break;
+            case '}':
+            case ']':
+                if (!$in_string) {
+                    $level--;
+                    $formated .= "\n" . str_repeat($tab, $level) . $char;
+                } else {
+                    $formated .= $char;
+                }
+                break;
+            case ',':
+                if (!$in_string) {
+                    $formated .= ",\n" . str_repeat($tab, $level);
+                } else {
+                    $formated .= $char;
+                }
+                break;
+            case ':':
+                if (!$in_string) {
+                    $formated .= ": ";
+                } else {
+                    $formated .= $char;
+                }
+                break;
+            case '"':
+                if ($c > 0 && $json[$c - 1] != '\\') {
+                    $in_string = !$in_string;
+                }
+            default:
+                $formated .= $char;
+                break;
+        }
+    }
+
+    return $formated;
+}
+
