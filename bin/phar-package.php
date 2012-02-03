9635e4daa179e5e6c1de606f19aa7d64c2489fc7
Henrique Moody <henriquemoody@gmail.com>
2012-02-03 20:29:33 -0200

    Update bin/phar-package.php with the functions of bin/common-inc.php.

--- bin/phar-package.php
+++ bin/phar-package.php
@@ -1,21 +1,24 @@
 <?php
 
-chdir(__DIR__);
-date_default_timezone_set('UTC');
+/* Common */
+require 'common-inc.php';
 
 $package_xml_file = '../package.xml';
 
-if (!file_exists($package_xml_file))
-    die("package.xml does not exists");
+if (!file_exists($package_xml_file)) {
+    writeln_error('"package.xml" does not exists');
+    exit(1);
+}
 
 $package_data = simplexml_load_file($package_xml_file);
-$project_name = $package_data->name;
-$phar_name = "$project_name.phar";
+$project_name = preg_replace('/[\/\\:.]/', '', $package_data->name);
+$phar_name = "{$project_name}.phar";
 
-$phar = new Phar("../$phar_name", 0, $phar_name);
+$phar = new Phar("../{$phar_name}", 0, $phar_name);
 
-foreach ($package_data->contents->dir->file as $file)
+foreach ($package_data->contents->dir->file as $file) {
     $phar->addFile(realpath("../{$file['name']}"), "{$file['baseinstalldir']}{$file['install-as']}");
+}
 
 $phar->addFromString("autoload.php", <<<'LOADER'
 <?php
@@ -37,3 +40,11 @@ LOADER
 );
 
 $phar->setStub("<?php Phar::mapPhar('$phar_name'); include 'phar://$phar_name/autoload.php'; __HALT_COMPILER();");
+
+writeln(array(
+    sprintf(
+        'Phar file "%s" created with success.', 
+        $phar_name
+    ),
+    $argv[0] . ': done!'
+));
