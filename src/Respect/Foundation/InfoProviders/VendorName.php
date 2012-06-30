dd6779add3f1fef552dd67b3b52f7e3b124d9a8e
nickl- <github@jigsoft.co.za>
2012-06-30 15:15:53 +0200

    Prevent FATAL Exception.
    It is not permitted for __toString to throw exceptions.
    Since it is plausable that there might not be any packages in the src folder
    and if there is no foler then DirectoryItterator will throw exceptin so
    best we prevent that from happening.

--- src/Respect/Foundation/InfoProviders/VendorName.php
+++ src/Respect/Foundation/InfoProviders/VendorName.php
@@ -6,11 +6,11 @@ use DirectoryIterator;
 
 class VendorName extends AbstractProvider
 {
-	public function providerFolderStructure()
-	{
-		$libraryFolder = new LibraryFolder($this->projectFolder);
-		foreach (new DirectoryIterator((string) $libraryFolder) as $vendor)
-			if (!$vendor->isDot() && $vendor->isDir())
-				return $vendor->getFileName();
-	}
+    public function providerFolderStructure()
+    {
+        if (file_exists($libraryFolder = new LibraryFolder($this->projectFolder)))
+            foreach (new DirectoryIterator($libraryFolder) as $vendor)
+                    if (!$vendor->isDot() && $vendor->isDir())
+                            return $vendor->getFileName();
+    }
 }
