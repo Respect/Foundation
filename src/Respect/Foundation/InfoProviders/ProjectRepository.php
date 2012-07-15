8ce3e845a34b5f46ad593a014db4c80a0ff04d85
nickl- <github@jigsoft.co.za>
2012-07-15 15:41:35 +0200

    You know you can do better than that.
    Besides the fact that ProjectRepository would fall down kicking and screaming if the repository has no remotes.
    If the remote is not labeled origin it doesn't mean there are no remotes,
    try a little harder before you claim it to be blank.

--- src/Respect/Foundation/InfoProviders/ProjectRepository.php
+++ src/Respect/Foundation/InfoProviders/ProjectRepository.php
@@ -6,10 +6,17 @@ class ProjectRepository extends AbstractProvider
     public function providerGitConfig()
     {
         $configfile = $this->projectFolder.'/.git/config';
-        if (file_exists($configfile)) {
-            $gitConfig = parse_ini_file($configfile, true);
-            return $gitConfig['remote origin']['url'];
-        }
+        if (file_exists($configfile))
+            if (false !== $gitConfig = parse_ini_file($configfile, true))
+                if (isset($gitConfig['remote origin']['url']))
+                    return $gitConfig['remote origin']['url'];
+                else
+                    foreach(array_filter(
+                        array_keys($gitConfig), function ($k) {
+                            return false !== strpos($k, 'remote');
+                        })
+                    as $key)
+                        return $gitConfig[$key]['url'];
         return '';
     }
 }
